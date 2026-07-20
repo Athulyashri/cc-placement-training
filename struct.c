4c0e63457dca3fb5af049a473d5668376e32f974
Athulyashri <athulyashri57@gmail.com>
2026-07-20 16:31:22 +0530

    Add student struct and CGPA filtering functionality
    Implement student structure and CGPA input/output logic

---
+++ struct.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+typedef struct student
+{
+    int reg_no;
+    char name[10];
+    float cgpa;
+} acer;
+
+int main()
+{
+    int n, i;
+
+    scanf("%d", &n);
+
+    acer a[n];
+
+    for(i = 0; i < n; i++)
+    {
+        scanf("%d %s %f", &a[i].reg_no, a[i].name, &a[i].cgpa);
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        if(a[i].cgpa >= 8.0 && a[i].cgpa <= 10.0)
+        {
+            printf("%s\n", a[i].name);
+        }
+    }
+
+    return 0;
+}
